04ba218706875a0de72613500f19ad3bba54f763
Godspower Maurice <111581268+SeasonLeague@users.noreply.github.com>
2024-09-04 01:14:45 +0100

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,9 +4,8 @@ export default function Contact() {
         <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
         <p className="mb-4">We'd love to hear from you! Here's how you can reach us:</p>
         <ul className="list-disc pl-5">
-          <li>Email: verbosetwomillion@gmail.com</li>
-          <li>Phone: (234) 912 788 4963</li>
-          <li>Address: 123 Plant Street, Green City, Nature State 12345</li>
+          <li>Email: ihechilurumchibuezebonagechibu@gmail.com</li>
+          <li>Address: London, United Kingdom</li>
         </ul>
       </div>
     );
